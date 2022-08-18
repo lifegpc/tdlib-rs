3d5fd4e9154f07c40d17dbdff030dcefbf4216c1
lifegpc <g1710431395@gmail.com>
2022-08-18 05:04:32 +0000

    Update

--- src/objects/mtproto_api/mod.rs
+++ src/objects/mtproto_api/mod.rs
@@ -4,5 +4,6 @@ pub mod constructors;
 #[allow(non_camel_case_types)]
 /// Available functions
 pub mod functions;
+#[allow(non_camel_case_types)]
 /// Available types
 pub mod types;
--- src/objects/mtproto_api/types/mod.rs
+++ src/objects/mtproto_api/types/mod.rs
@@ -12,3 +12,14 @@ pub enum ResPQ {
     /// Response
     ResPQ(Box<resPQ>),
 }
+
+#[derive(
+    Clone, Debug, tdlib_rs_impl::OptDeserialize, tdlib_rs_impl::From1, tdlib_rs_impl::Serialize,
+)]
+/// The inner data in [Step 4](https://core.telegram.org/mtproto/auth_key#presenting-proof-of-work-server-authentication).
+pub enum P_Q_inner_data {
+    /// Used to create permanent authorization keys.
+    P_Q_inner_data_dc(Box<p_q_inner_data_dc>),
+    /// Used to create temporary authorization keys.
+    P_Q_inner_data_temp_dc(Box<p_q_inner_data_temp_dc>),
+}
